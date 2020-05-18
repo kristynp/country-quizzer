d1f5fd326edbf82c14ae758d445ce04914856940
github username <github email address>
2020-05-17 18:34:47 -0600

    added functionality to hide or show wordbank

--- country-quizzer-frontend/index.js
+++ country-quizzer-frontend/index.js
@@ -1,8 +1,30 @@
+const countries_url = "http://localhost:3000/api/v1/countries"
+
 document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('wordbank-btn').addEventListener('click', renderWordbank);
+  document.getElementById('wordbank-btn').addEventListener('click', handleWordbank);
 });
 
-function renderWordbank(){
-  console.log('pressed button');
+function handleWordbank(){
+  let button = document.getElementById('wordbank-btn');
+  let wordbankUl = document.getElementById('wordbank-ul');
   
+  if (button.innerHTML === "Use Wordbank") {
+    fetch(countries_url)
+    .then(response => response.json())
+    .then(countries => {
+      countries.data.forEach(country => {
+        let li = document.createElement('li');
+        li.innerHTML = country.attributes.name;
+        wordbankUl.appendChild(li);
+      })
+      button.innerHTML = "Hide Wordbank"
+    })
+  }  else {
+    while (wordbankUl.firstChild) {
+      wordbankUl.removeChild(wordbankUl.firstChild);
+    }
+    button.innerHTML = "Use Wordbank";
+  }
 }
+
+
